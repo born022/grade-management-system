9bd47d9ecdc0a28dd20d6bcc5b5e0f918299b92c
Melbourne Salonga <melbourne.salonga@deped.gov.ph>
2024-10-23 18:01:16 +0800

    Update database.php

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php
+// database.php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "grade_management_system";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
